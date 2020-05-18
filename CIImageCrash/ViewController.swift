ccc2e878c4d55d3eaf3ccaf3e2d53b7d59a704d1
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-05-18 14:33:12 -0300

    Initial Commit

---
+++ CIImageCrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIImageCrash
+//
+//  Created by Jeffrey Blagdon on 2020-05-18.
+//  Copyright © 2020 polyergy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
